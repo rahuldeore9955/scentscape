a47063d1a8313546c1e5690571981be021748ae8
Rahul Deore <rahuldeore9955@gmail.com>
2026-09-15 07:25:25 +0530

    Improve guest checkout modal layout

--- resources/views/checkout/guest-modal.blade.php
+++ resources/views/checkout/guest-modal.blade.php
@@ -10,12 +10,16 @@
             <form id="guestCheckoutForm" class="checkout-form">
                 <input type="hidden" name="product_id" id="checkoutProductId">
                 <input type="hidden" name="quantity" id="checkoutQuantity" value="1">
+                <div class="checkout-form-section-title">Account details</div>
                 <div class="checkout-form-grid">
                     <label>Name<input type="text" name="name" required autocomplete="name"></label>
                     <label>Email<input type="email" name="email" required autocomplete="email"></label>
                     <label>Mobile Number<input type="tel" name="phone" inputmode="numeric" pattern="[0-9]{10}" maxlength="10" placeholder="9876543210" required autocomplete="tel"></label>
                     <label>Password<input type="password" name="password" minlength="6" required autocomplete="new-password"></label>
                     <label>Confirm Password<input type="password" name="password_confirmation" minlength="6" required autocomplete="new-password"></label>
+                </div>
+                <div class="checkout-form-section-title">Delivery address</div>
+                <div class="checkout-form-grid">
                     <label class="checkout-full-field">Address Line 1<input type="text" name="address_line1" required autocomplete="street-address"></label>
                     <label class="checkout-full-field">Address Line 2<input type="text" name="address_line2" autocomplete="address-line2"></label>
                     <label>State<input type="text" name="state" required autocomplete="address-level1"></label>
